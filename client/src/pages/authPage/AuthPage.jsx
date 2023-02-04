7939a79703d8bc85b599feaa9d7958533b6c98af
Ilya Gurinovich <gurinovich-ilya@list.ru>
2023-02-04 23:33:13 +0300

    add authPage and fonts

--- client/src/pages/authPage/AuthPage.jsx
+++ client/src/pages/authPage/AuthPage.jsx
@@ -1,17 +1,36 @@
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import { LOG_ROUTE, REG_ROUTE } from '../../utils/routes'
 
 import classes from './AuthPage.module.css'
 
 const AuthPage = () => {
+  const loginPage = useLocation().pathname.split('/').reverse()[0];
   return (
     <div className={classes.container}>
-      <div className={classes.loginCard}>
-        <form>
-          <input placeholder='Введите почту'/>
-          <input placeholder='Введите пароль'/>
-          <button>Авторизироваться</button>
+      {
+        loginPage !== 'register'
+        ?
+        <form className={classes.loginCard}>
+          <p className={classes.txt}>Электронная почта:</p>
+          <input className={classes.inp} placeholder='Введите почту'/>
+          <p className={classes.txt}>Пароль:</p>
+          <input className={classes.inp} placeholder='Введите пароль'/>
+          <p className={classes.link}>Нет аккаунта? <Link to={REG_ROUTE}> Зарегистрироваться</Link></p>
+          <button className={classes.but}>Авторизироваться</button>
         </form>
-      </div>
+        :
+        <form className={classes.loginCard}>
+          <p className={classes.txt}>Никнейм:</p>
+          <input className={classes.inp} placeholder='Введите никнейм'/>
+          <p className={classes.txt}>Электронная почта:</p>
+          <input className={classes.inp} placeholder='Введите почту'/>
+          <p className={classes.txt}>Пароль:</p>
+          <input className={classes.inp} placeholder='Введите пароль'/>
+          <p className={classes.link}>Есть аккаунт?<Link to={LOG_ROUTE}> Авторизироваться</Link></p>
+          <button className={classes.but}>Зарегистрироваться</button>
+        </form>
+      }
     </div>
   )
 }
