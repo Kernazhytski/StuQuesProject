64548bc5ef34534ec5a5e8dfec2ab4be11a8b03c
Andrei Kernazhytski <108015888+Kernazhytski@users.noreply.github.com>
2023-03-12 13:59:01 +0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -10,10 +10,6 @@ export const Context = createContext({
     store, 
 })
 
-app.listen(5000, () => {
-	console.log('App listening on port 5000')
-})
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Context.Provider value={{store}}>
